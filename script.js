06b20a770cb835310fd2ccb49b2d26ac7228d9d1
jamesdollman <dollmanjamie@gmail.com>
2023-10-10 17:58:35 +1100

    began creating divs

---
+++ script.js
@@ -0,0 +1,16 @@
+const gridContainer = document.querySelector('.gridContainer');
+
+//create divs
+const div1 = document.createElement('div');
+
+//give class names 
+div1.className = "div1";
+
+//append divs to gridContainer
+gridContainer.appendChild(div1);
+
+function createGrid() {
+    for(let i = 0; i<16; i++) {
+        
+    }
+}
