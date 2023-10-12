02f482c3a14a91343a1c05b978cbe9c9103b88a3
jamesdollman <dollmanjamie@gmail.com>
2023-10-12 21:55:25 +1100

    tried removing prompt window

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 const gridContainer = document.querySelector('.gridContainer');
 const body = document.querySelector('body');
+const inputContainer = document.querySelector('.inputContainer');
 let header;
 let box;
 let resetButton;
-let NumberOfSquares;
+let NumberOfSquares = 0;
 let clicked = false;
 let squareSize;
+let input;
+let inputButton;
 
 function getSquareSize() {
-    while(true){
-        NumberOfSquares = prompt("How many squares do you want per side?");
-        if(NumberOfSquares > 0 && NumberOfSquares <= 64){
-            break;
-        }else{}
-    }   
-    squareSize = (100 / NumberOfSquares) + "%";
+    input = document.createElement('input');
+    input.className = "userInput";
+    input.textContent = "How many squares do you want per side?";
+    inputContainer.appendChild(input);
+    inputButton = document.createElement('button');
+    inputButton.className = "inputButton";
+    inputButton.textContent = "Press me!";
+    inputContainer.appendChild(inputButton);
+    
+    inputButton.addEventListener('click', () => {
+        NumberOfSquares = input.value;
+            if(NumberOfSquares > 0 && NumberOfSquares <= 64){
+                squareSize = (100 / NumberOfSquares) + "%";
+                console.log(squareSize);
+                displayElements(); 
+            }else if (NumberOfSquares > 64 || NumberOfSquares < 1){
+                input.textContent = "";
+                console.log("ERROR");
+            }
+        
+    })
+        
+
+    
 }
 
 
@@ -22,7 +42,7 @@ drawGame();
 
 function drawGame() {
     getSquareSize();
-    displayElements();   
+      
 }
 
 function displayElements(){
@@ -60,11 +80,15 @@ function createResetButton(){
 
 function resetBoard() {
     resetButton.addEventListener('click', () => {
+        input.value = 0;
         const colouredBoxes = document.querySelectorAll('.visited');
         colouredBoxes.forEach(box => {
             box.classList.remove("visited");
+            box.remove();
         });
     });
+
+    
     
 }
 
