76de250546ca8b59b7e420f74acc57b9ea13080f
jamesdollman <dollmanjamie@gmail.com>
2023-10-12 21:12:53 +1100

    added click to start drawing

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 const gridContainer = document.querySelector('.gridContainer');
 const body = document.querySelector('body');
+let header;
 let box;
 let resetButton;
+let NumberOfSquares;
+let clicked = false;
+let squareSize;
+
+function getSquareSize() {
+    while(true){
+        NumberOfSquares = prompt("How many squares do you want per side?");
+        if(NumberOfSquares > 0 && NumberOfSquares <= 64){
+            break;
+        }else{}
+    }   
+    squareSize = (100 / NumberOfSquares) + "%";
+}
+
 
 drawGame();
 
-function createDiv(){
-    for(let i = 0; i < 256; i++){
+function drawGame() {
+    getSquareSize();
+    displayElements();   
+}
+
+function displayElements(){
+    for(let i = 0; i < NumberOfSquares*NumberOfSquares; i++){
         box = document.createElement('div');
         box.className = (i+1).toString();
+        box.style.width = squareSize;
+        box.style.height = squareSize;
         gridContainer.appendChild(box);
+        box.addEventListener('click', () =>{
+            startDrawing();
+            clicked = true;
+        })
+
             box.addEventListener('mouseover', (event) => {
-                event.target.classList.add("visited");
+                if(clicked){
+                    event.target.classList.add("visited");
+                }
                }) 
-   }   
+        }
+   createResetButton();
+}
+
+function startDrawing() {
+    
 }
 
 function createResetButton(){
@@ -34,14 +68,6 @@ function resetBoard() {
     
 }
 
-function pixelatedEffect() {
-    
-}
 
-function drawGame() {
-    createDiv();
-    createResetButton();
-    
-}
 
 
